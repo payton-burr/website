1915cfb62051eee19b22328c5ed4e86b35cecf1a
Payton Burr <paytonburr@outlook.com>
2022-09-24 22:14:20 -0600

    imported Navigation component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import "../styles/globals.css";
+import Navigation from "../ui/Navigation";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="container">
+      <Navigation />
       <main>
         <Component {...pageProps} />
       </main>
