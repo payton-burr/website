b9335c3f11245e7a71eee2b259277b3f9189295e
Payton Burr <paytonburr@outlook.com>
2022-10-02 21:02:37 -0600

    converted Home component to use tailwind styling

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,40 @@
 import { allPosts as posts } from "contentlayer/generated";
 import Link from "next/link";
-import Head from "next/head";
+import Image from "next/image";
 
 export async function getStaticProps() {
   return { props: { posts } };
 }
 
-function PostCard(post) {
-  return (
-    <div className="mb-6">
-      <h2 className="text-lg">
-        <Link href={post.url}>
-          <a className="text-blue-700 hover:text-blue-900">{post.title}</a>
-        </Link>
-      </h2>
-    </div>
-  );
-}
-
 export default function Home({ posts }) {
-  console.log(posts);
   return (
-    <div className="mx-auto max-w-2xl py-16 text-center">
-      <Head>
-        <title>Contentlayer Blog Example</title>
-      </Head>
-
-      <h1 className="mb-8 text-3xl font-bold">Contentlayer Blog Example</h1>
-
+    <div className="mt-12">
       {posts.map((post, index) => (
-        <PostCard key={index} {...post} />
+        <Link href={post.url} passHref key={index}>
+          <div className="relative flex flex-col min-w-0 rounded break-words border bg-white border-gray-300 mb-3 cursor-pointer max-w-xl">
+            <div className="flex flex-wrap g-0">
+              <div className="md:w-2/3 pr-4 pl-3">
+                <div className="flex-auto pt-6">
+                  <h5 className="mb-3 font-semibold text-xl">{post.title}</h5>
+                  <p className="mb-0">A post about stuff</p>
+                  <p className="mb-0">
+                    <small className="text-gray-700">Oct 2nd</small>
+                  </p>
+                </div>
+              </div>
+              <div className="md:w-1/3 pl-4 m-auto">
+                <Image
+                  src={post.image}
+                  className="max-w-full h-auto mt-1 rounded-start"
+                  alt="thumbnail"
+                  width={500}
+                  height={400}
+                  objectFit="cover"
+                />
+              </div>
+            </div>
+          </div>
+        </Link>
       ))}
     </div>
   );
