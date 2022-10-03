b31cec5d1bb50549660b3a94391e5d749e71b935
Payton Burr <paytonburr@outlook.com>
2022-10-02 20:59:36 -0600

    added image domain for fetching external images

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
