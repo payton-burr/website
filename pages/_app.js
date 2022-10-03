e4af18320a43c2610e596499f1dfa5a75d429c1d
Payton Burr <paytonburr@outlook.com>
2022-10-02 21:01:18 -0600

    wrapped Component in other markup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Navigation from "../ui/Navigation";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto mt-5 px-20">
+      <Navigation />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
