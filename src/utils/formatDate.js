05f4bf1e4699ecf1659f52d5f0dd464d1ff99f58
Payton Burr <paytonburr@outlook.com>
2023-02-06 22:40:43 -0700

    refactor: formatDate utils to use date-fns features

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,7 @@
+import { format, isThisYear } from "date-fns";
+
 export function formatDate(date) {
-  return new Date(date).toLocaleDateString("en-US", {
-    timeZone: "UTC",
-    month: "short",
-    day: "numeric",
-  });
+  const _date = new Date(date);
+
+  return isThisYear(_date) ? format(_date, "MMM d") : format(_date, "MMM d, y");
 }
