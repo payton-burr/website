0631be263161b5d1eaa60da8c9e3c9f8eff27290
Payton Burr <paytonburr@outlook.com>
2023-03-13 23:54:39 -0600

    chore: zod type definitions

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    // date: z.date(),
+    description: z.string().max(160),
+    tags: z.array(z.string()),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
