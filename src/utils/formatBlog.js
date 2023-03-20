f3552b8f4e519e69cfef443ec3ba419093267baf
Payton Burr <paytonburr@outlook.com>
2023-03-19 19:37:59 -0600

    refactor: formatBlog utils to filter drafts

--- src/utils/formatBlog.js
+++ src/utils/formatBlog.js
@@ -1,5 +1,6 @@
 export function formatBlog(posts) {
-  return [...posts].sort(
-    (a, b) => new Date(b.data.date) - new Date(a.data.date)
-  );
+  console.log(posts);
+  return [...posts]
+    .sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
+    .filter((post) => !post.data.draft);
 }
