3f7eec7cfda4d62a83aaa2ff1f55a7d9499a8066
Payton Burr <paytonburr@outlook.com>
2023-03-19 22:33:13 -0600

    chore: cleanup

--- src/utils/formatBlog.js
+++ src/utils/formatBlog.js
@@ -1,5 +1,4 @@
 export function formatBlog(posts) {
-  console.log(posts);
   return [...posts]
     .sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
     .filter((post) => !post.data.draft);
