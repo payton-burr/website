429db4bd345194f9ef607c919a85a45b8982ecd2
Payton Burr <paytonburr@outlook.com>
2023-03-30 22:55:17 -0600

    style(ui): responsive styles

--- src/ui/Project.jsx
+++ src/ui/Project.jsx
@@ -1,7 +1,7 @@
 function Project({ children }) {
   // console.log(children);
   return (
-    <div className="not-prose bg-zinc-800 m-4 p-4 rounded-md max-w-xs">
+    <div className="not-prose bg-zinc-800 m-4 p-4 rounded-md max-w-xs md:max-w-lg md:flex">
       {children}
     </div>
   );
