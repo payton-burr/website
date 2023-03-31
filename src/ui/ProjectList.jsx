ed928536be00e20b2eb2f6e45afdf849045b78a0
Payton Burr <paytonburr@outlook.com>
2023-03-30 22:58:32 -0600

    refactor(Project): markup and styles

--- src/ui/ProjectList.jsx
+++ src/ui/ProjectList.jsx
@@ -15,9 +15,28 @@ function ProjectList() {
         setCategory={setCategory}
         setFilteredProjects={setFilteredProjects}
       />
-      <div className="flex justify-start flex-wrap">
+      <div>
         {filteredProjects.map((project) => {
-          return <Project key={project.id}>{project.title}</Project>;
+          return (
+            <Project key={project.id}>
+              <img
+                src="/mountain.jpg"
+                alt=""
+                className="inline w-full rounded md:w-36 md:mr-4"
+              />
+              <div className="content">
+                <p className="text-xl my-5 leading-3">
+                  An Above Average Experience
+                </p>
+                <p className="text-sm my-3">
+                  Travel to the premiere mountain tops of New Zealand with our
+                  guided tours. Take in the majestic scenary and witness beauty.
+                </p>
+                {project.links.github}
+                {project.links.preview}
+              </div>
+            </Project>
+          );
         })}
       </div> */}
     </div>
