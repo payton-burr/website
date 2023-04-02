51a240026fff650b51cb52499ed8db94ee441cca
Payton Burr <paytonburr@outlook.com>
2023-04-02 17:04:40 -0600

    build(tailwind): fontFamily config for future use

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme.js";
 import typography from "@tailwindcss/typography";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { display: [...defaultTheme.fontFamily.sans] },
+    },
   },
   plugins: [typography],
 };
