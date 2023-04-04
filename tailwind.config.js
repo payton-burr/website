134ac97f694353f200ea3cdc60d4774111a3add8
Payton Burr <paytonburr@outlook.com>
2023-04-03 22:40:32 -0600

    build(tailwind): darkmode class

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ import typography from "@tailwindcss/typography";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: { display: [...defaultTheme.fontFamily.sans] },
