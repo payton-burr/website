09a23f6a3e473c379b2e295e1e2c58e7bee5ff81
Payton Burr <paytonburr@outlook.com>
2023-04-22 18:26:50 -0600

    Use qouteless typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
+      typography: {
+        quoteless: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+          },
+        },
+      },
       fontFamily: { display: [...defaultTheme.fontFamily.sans] },
     },
   },
