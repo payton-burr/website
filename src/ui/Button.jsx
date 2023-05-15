3ee27f656f4f44a6affe52f3b7517d663949d9ab
Payton Burr <paytonburr@outlook.com>
2023-05-14 22:42:38 -0600

    Tweak button colors

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,7 +2,7 @@ function Button({ children, ...rest }) {
   return (
     <button
       {...rest}
-      className="bg-slate-200 dark:bg-zinc-800 py-1 px-2 rounded-sm text-xs md:text-sm mr-1 mt-1"
+      className="bg-slate-100 dark:bg-zinc-800/20 py-1 px-2 rounded-sm text-xs md:text-sm mr-1 mt-1"
     >
       {children}
     </button>
