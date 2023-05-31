30656af9c25b931afaacd060d871c5aed8a2dc95
Payton Burr <paytonburr@outlook.com>
2023-05-30 22:46:56 -0600

    Cleanup

--- src/ui/Project.jsx
+++ src/ui/Project.jsx
@@ -1,5 +1,4 @@
 function Project({ children }) {
-  // console.log(children);
   return (
     <section className="not-prose bg-slate-100 dark:bg-zinc-800/20 my-4 p-4 rounded-md max-w-xs md:min-w-full md:flex">
       {children}
