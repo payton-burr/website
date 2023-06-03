58ab0c7e40a186475a304d9e5b77a67a4f779f3f
Payton Burr <paytonburr@outlook.com>
2023-06-03 16:57:41 -0600

    Change output

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,9 @@ export default defineConfig({
     },
   },
   integrations: [tailwind(), react()],
-  output: "server",
+  output: "hybrid",
+  experimental: {
+    hybridOutput: true,
+  },
   adapter: vercel(),
 });
