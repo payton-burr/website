102fc5fa7277216e44d1cd9af688db64bea8a182
Payton Burr <paytonburr@outlook.com>
2023-08-06 17:29:22 -0600

    Change Project to astro component

--- src/ui/Project.jsx
+++
@@ -1,9 +0,0 @@
-function Project({ children }) {
-  return (
-    <section className="not-prose bg-slate-100 dark:bg-zinc-800/20 my-4 p-4 rounded-md max-w-xs md:min-w-full md:flex">
-      {children}
-    </section>
-  );
-}
-
-export default Project;
