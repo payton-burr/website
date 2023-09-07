9a122d2ecd27ed8145f51621f66ae624c3af3dd1
Payton Burr <paytonburr@outlook.com>
2023-09-07 17:15:54 -0600

    Change output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +10,4 @@ export default defineConfig({
     },
   },
   integrations: [tailwind(), react()],
-  output: "hybrid",
-  adapter: vercel(),
 });
